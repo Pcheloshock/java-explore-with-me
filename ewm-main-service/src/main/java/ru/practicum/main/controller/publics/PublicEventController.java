f6a41046948208cf6ad52ad96998f05be09c5f92
pcheloshock <kraniys@yandex.ru>
2026-03-24 15:06:34 +0500

    diplom_proekt 0.0.6

---
+++ ewm-main-service/src/main/java/ru/practicum/main/controller/publics/PublicEventController.java
@@ -0,0 +1,42 @@
+package ru.practicum.main.controller.publics;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.main.dto.EventFullDto;
+import ru.practicum.main.dto.EventShortDto;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/events")
+@RequiredArgsConstructor
+public class PublicEventController {
+    
+    @GetMapping
+    public List<EventShortDto> getEvents(
+            @RequestParam(required = false) String text,
+            @RequestParam(required = false) List<Long> categories,
+            @RequestParam(required = false) Boolean paid,
+            @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime rangeStart,
+            @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime rangeEnd,
+            @RequestParam(defaultValue = "false") Boolean onlyAvailable,
+            @RequestParam(required = false) String sort,
+            @RequestParam(defaultValue = "0") int from,
+            @RequestParam(defaultValue = "10") int size,
+            HttpServletRequest request) {
+        log.info("GET /events");
+        return new ArrayList<>();
+    }
+    
+    @GetMapping("/{id}")
+    public EventFullDto getEventById(@PathVariable Long id, HttpServletRequest request) {
+        log.info("GET /events/{}", id);
+        throw new ru.practicum.main.exception.NotFoundException("Event not found");
+    }
+}
