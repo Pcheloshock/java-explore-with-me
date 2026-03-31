dda00ea4adf85346d573e60c5f46a73819960cf7
pcheloshock <kraniys@yandex.ru>
2026-03-31 22:32:43 +0500

    diplom_projekt_demon 0.0.9

--- ewm-main-service/src/main/java/ru/practicum/main/dto/NewEventDto.java
+++ ewm-main-service/src/main/java/ru/practicum/main/dto/NewEventDto.java
@@ -1,56 +1,48 @@
 package ru.practicum.main.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.*;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class NewEventDto {
-    
-    @NotBlank(message = "Annotation cannot be blank")
-    @Size(min = 20, max = 2000, message = "Annotation must be between 20 and 2000 characters")
+    @NotBlank
+    @Size(min = 20, max = 2000)
     private String annotation;
 
-    @NotNull(message = "Category cannot be null")
+    @NotNull
     private Long category;
 
-    @NotBlank(message = "Description cannot be blank")
-    @Size(min = 20, max = 7000, message = "Description must be between 20 and 7000 characters")
+    @NotBlank
+    @Size(min = 20, max = 7000)
     private String description;
 
-    @NotBlank(message = "Event date cannot be blank")
-    private String eventDate;
+    @NotNull
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime eventDate;
 
-    @NotNull(message = "Location cannot be null")
+    @NotNull
     private LocationDto location;
 
-    private Boolean paid = false;
+    private Boolean paid;
 
-    @PositiveOrZero(message = "Participant limit must be 0 or positive")
-    private Integer participantLimit = 0;
+    private Integer participantLimit;
 
-    private Boolean requestModeration = true;
+    private Boolean requestModeration;
 
-    @NotBlank(message = "Title cannot be blank")
-    @Size(min = 3, max = 120, message = "Title must be between 3 and 120 characters")
+    @NotBlank
+    @Size(min = 3, max = 120)
     private String title;
-    
+
     public LocalDateTime getEventDateAsLocalDateTime() {
-        try {
-            return LocalDateTime.parse(eventDate, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-        } catch (DateTimeParseException e) {
-            try {
-                return LocalDateTime.parse(eventDate, DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss"));
-            } catch (DateTimeParseException ex) {
-                return LocalDateTime.parse(eventDate, DateTimeFormatter.ISO_LOCAL_DATE_TIME);
-            }
-        }
+        return eventDate;
     }
 }
--- ewm-main-service/src/main/java/ru/practicum/main/dto/UpdateEventAdminRequest.java
+++ ewm-main-service/src/main/java/ru/practicum/main/dto/UpdateEventAdminRequest.java
@@ -1,56 +1,34 @@
 package ru.practicum.main.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import ru.practicum.main.model.AdminStateAction;
 
-import javax.validation.constraints.*;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class UpdateEventAdminRequest {
-    
-    @Size(min = 20, max = 2000)
     private String annotation;
-    
     private Long category;
-    
-    @Size(min = 20, max = 7000)
     private String description;
-    
-    private String eventDate;
-    
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime eventDate;
+
     private LocationDto location;
-    
     private Boolean paid;
-    
-    @PositiveOrZero
     private Integer participantLimit;
-    
     private Boolean requestModeration;
-    
-    private AdminStateAction stateAction;
-    
-    @Size(min = 3, max = 120)
     private String title;
-    
+    private AdminStateAction stateAction;
+
     public LocalDateTime getEventDateAsLocalDateTime() {
-        if (eventDate == null) {
-            return null;
-        }
-        try {
-            return LocalDateTime.parse(eventDate, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-        } catch (DateTimeParseException e) {
-            try {
-                return LocalDateTime.parse(eventDate, DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss"));
-            } catch (DateTimeParseException ex) {
-                return LocalDateTime.parse(eventDate, DateTimeFormatter.ISO_LOCAL_DATE_TIME);
-            }
-        }
+        return eventDate;
     }
 }
